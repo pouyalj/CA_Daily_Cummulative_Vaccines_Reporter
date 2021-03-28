7c463b315e2fc9e721cf819ff6b964872addfd54
Pouya Lajevardi <pouya.lajevardi@mail.utoronto.ca>
2021-03-28 17:39:45 -0400

    Add files via upload

--- ON_Covid_Vaccine.py
+++ ON_Covid_Vaccine.py
@@ -26,32 +26,59 @@ ON_vaccine_data = vaccine_Data.loc[vaccine_Data['province'] == "Ontario"]
 
 
 
-values = pd.date_range(start="14-12-2020",end=datetime.date.today(), periods = 18)
+values = pd.date_range(start="14-12-2020",end=datetime.date.today(), periods = 25)
 
+
+
+print("Last 2 days stats are: \n"
+    + str(ON_vaccine_data["date_vaccine_administered"].values[-2]) + "  " +  str(ON_vaccine_data["avaccine"].values[-2]) + "\n"
+ + str(ON_vaccine_data["date_vaccine_administered"].values[-1]) + "  " + str(ON_vaccine_data["avaccine"].values[-1]))
 
 
 fig = plt.figure(figsize=(24, 12))
-plt.plot(ON_vaccine_data["date_vaccine_administered"], 1 * ON_vaccine_data["avaccine"], label="Administered vaccine on the day")
-plt.xticks(ON_vaccine_data["date_vaccine_administered"]), values
-plt.locator_params(axis='x', nbins=13)
-plt.title("Ontario Vaccine administration data", fontsize=22)
-plt.xlabel('Date', fontsize=18)
-plt.ylabel('Shots Administered', fontsize=20)
+plt.plot(ON_vaccine_data["date_vaccine_administered"], 1 * ON_vaccine_data["avaccine"], label="Administered Shots On The Day",  linewidth=2, color="black")
+plt.xticks(ON_vaccine_data["date_vaccine_administered"], fontsize=16)
+plt.yticks(fontsize=14)
+plt.rc('font', size=14)
+plt.locator_params(axis='x', nbins=18)
+plt.locator_params(axis='y', nbins=9)
+plt.title("Ontario Daily Vaccine Administration Data", fontsize=22, color="black")
+plt.xlabel('Date', fontsize=18, color="black")
+plt.ylabel('Shots Administered', fontsize=20, color="black")
 plt.ticklabel_format(axis="y", style="sci", scilimits=(3,3))
 fig.autofmt_xdate()
+# fig.patch.set_facecolor('black')
+# fig.patch.set_alpha(1)
+ax = plt.axes()
+# ax.patch.set_facecolor("gray")
+# ax.patch.set_alpha(0.6)
+ax.tick_params(axis='x', colors='black')
+ax.tick_params(axis='y', colors='black')
+ax.yaxis.offsetText.set_fontsize(16)
 plt.legend()
 plt.savefig('ON_Vaccine_Graph by Day.png')
 
 
 
 fig = plt.figure(figsize=(24, 12))
-plt.plot(ON_vaccine_data["date_vaccine_administered"] , ON_vaccine_data["cumulative_avaccine"], label="Cumulative vaccine administered")
-plt.xticks(ON_vaccine_data["date_vaccine_administered"]), values
-plt.locator_params(axis='x', nbins=13)
-plt.title("Ontario Vaccine administration data", fontsize=22)
-plt.xlabel('Date', fontsize=18)
-plt.ylabel('Shots Administered', fontsize=20)
+plt.plot(ON_vaccine_data["date_vaccine_administered"] , ON_vaccine_data["cumulative_avaccine"], label="Cumulative Shots Administered", linewidth=2, color="black")
+plt.xticks(ON_vaccine_data["date_vaccine_administered"], fontsize=16)
+plt.yticks(fontsize=14)
+plt.rc('font', size=14)
+plt.locator_params(axis='x', nbins=18)
+plt.locator_params(axis='y', nbins=9)
+plt.title("Ontario Cumulative Vaccine Administration Data", fontsize=22, color="black")
+plt.xlabel('Date', fontsize=18, color="black")
+plt.ylabel('Shots Administered', fontsize=20, color="black")
 plt.ticklabel_format(axis="y", style="sci", scilimits=(6,6))
 fig.autofmt_xdate()
+# fig.patch.set_facecolor('black')
+# fig.patch.set_alpha(1)
+ax = plt.axes()
+# ax.patch.set_facecolor("gray")
+# ax.patch.set_alpha(0.6)
+ax.tick_params(axis='x', colors='black')
+ax.tick_params(axis='y', colors='black')
+ax.yaxis.offsetText.set_fontsize(16)
 plt.legend()
-plt.savefig('ON_Vaccine_Graph Cummulative.png')
+plt.savefig('ON_Vaccine_Graph Cumulative.png')
